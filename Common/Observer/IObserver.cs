33de664ca6c10625a5824f15702a1575fd676b09
Abner Lima <abner.lima27@outlook.com>
2020-01-18 02:22:27 -0300

    Observer Created

---
+++ Common/Observer/IObserver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Observer
+{
+    public interface IObserver
+    {
+        void Update<T>(INotification<T> notification);
+    }
+}
---
+++ Common/Observer/Observable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Observer
+{
+    public abstract class Observable : IObservable
+    {
+        private ICollection<IObserver> observers;
+
+        public ICollection<IObserver> Observers
+        {
+            get
+            {
+                if (this.observers == null)
+                    this.observers = new List<IObserver>();
+                return this.observers;
+            }
+        }
+
+        public void AddObserver(IObserver observer)
+        {
+            this.Observers.Add(observer);
+        }
+
+        public void Notify<T>(INotification<T> notification)
+        {
+            foreach(IObserver observer in Observers)
+            {
+                observer.Update(notification);
+            }
+        }
+    }
+}
