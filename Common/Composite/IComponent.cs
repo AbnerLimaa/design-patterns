a9e0c9e0ec2c21446fd8c6041fba31dd569822cc
Abner Lima <abner.lima27@outlook.com>
2020-02-18 23:11:06 -0300

    Composite

---
+++ Common/Composite/IComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Composite
+{
+    public interface IComponent
+    {
+        IOperationResponse Operation(Func<ICollection<IComponent>, IOperationArgument[], IOperationResponse> func, params IOperationArgument[] list);
+    }
+}
