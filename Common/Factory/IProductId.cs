acfa94a521c258680038705e79ef279c70b08c7d
Abner Lima <abner.lima27@outlook.com>
2020-02-18 22:35:11 -0300

    Factory, Prototype and Singleton

---
+++ Common/Factory/IProductId.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Factory
+{
+    public interface IProductId
+    {
+        string Id { get; }
+    }
+}
---
+++ Common/Factory/ProductId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Factory
+{
+    public class ProductId : IProductId
+    {
+        public string Id { get; }
+
+        public ProductId(string id)
+        {
+            this.Id = id;
+        }
+    }
+}
